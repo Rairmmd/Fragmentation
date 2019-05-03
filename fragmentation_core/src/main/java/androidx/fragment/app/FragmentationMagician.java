f78a98197188a918084ae50a3e54a547eee39a7b
RairYu <rairmmd@qq.com>
2019-05-03 23:26:02 +0800

    update to androidx

--- fragmentation_core/src/main/java/androidx/fragment/app/FragmentationMagician.java
+++ fragmentation_core/src/main/java/androidx/fragment/app/FragmentationMagician.java
@@ -1,4 +1,4 @@
-package android.support.v4.app;
+package androidx.fragment.app;
 
 
 import android.util.SparseArray;
@@ -6,7 +6,9 @@ import android.util.SparseArray;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * http://stackoverflow.com/questions/23504790/android-multiple-fragment-transaction-ordering
@@ -35,8 +37,9 @@ public class FragmentationMagician {
     }
 
     public static boolean isExecutingActions(FragmentManager fragmentManager) {
-        if (!(fragmentManager instanceof FragmentManagerImpl))
+        if (!(fragmentManager instanceof FragmentManagerImpl)) {
             return false;
+        }
         try {
             FragmentManagerImpl fragmentManagerImpl = (FragmentManagerImpl) fragmentManager;
             return fragmentManagerImpl.mExecutingActions;
@@ -52,14 +55,18 @@ public class FragmentationMagician {
      */
     @SuppressWarnings("unchecked")
     public static void reorderIndices(FragmentManager fragmentManager) {
-        if (!sSupportLessThan25dot4) return;
-        if (!(fragmentManager instanceof FragmentManagerImpl))
+        if (!sSupportLessThan25dot4) {
             return;
+        }
+        if (!(fragmentManager instanceof FragmentManagerImpl)) {
+            return;
+        }
         try {
             FragmentManagerImpl fragmentManagerImpl = (FragmentManagerImpl) fragmentManager;
             Object object = getValue(fragmentManagerImpl, "mAvailIndices");
-            if (object == null) return;
-
+            if (object == null) {
+                return;
+            }
             ArrayList<Integer> arrayList = (ArrayList<Integer>) object;
             if (arrayList.size() > 1) {
                 Collections.sort(arrayList, Collections.reverseOrder());
@@ -70,8 +77,9 @@ public class FragmentationMagician {
     }
 
     public static boolean isStateSaved(FragmentManager fragmentManager) {
-        if (!(fragmentManager instanceof FragmentManagerImpl))
+        if (!(fragmentManager instanceof FragmentManagerImpl)) {
             return false;
+        }
         try {
             FragmentManagerImpl fragmentManagerImpl = (FragmentManagerImpl) fragmentManager;
             return fragmentManagerImpl.mStateSaved;
@@ -141,10 +149,13 @@ public class FragmentationMagician {
      */
     @SuppressWarnings("unchecked")
     public static List<Fragment> getActiveFragments(FragmentManager fragmentManager) {
-        if (!(fragmentManager instanceof FragmentManagerImpl))
+        if (!(fragmentManager instanceof FragmentManagerImpl)) {
             return Collections.EMPTY_LIST;
+        }
         // For pre-25.4.0
-        if (sSupportLessThan25dot4) return fragmentManager.getFragments();
+        if (sSupportLessThan25dot4) {
+            return fragmentManager.getFragments();
+        }
 
         // For compat 25.4.0+
         try {
@@ -158,19 +169,19 @@ public class FragmentationMagician {
     }
 
     @SuppressWarnings("unchecked")
-    private static List<Fragment> getActiveList(SparseArray<Fragment> active) {
+    private static List<Fragment> getActiveList(HashMap<String, Fragment> active) {
         if (active == null) {
             return Collections.EMPTY_LIST;
         }
         final int count = active.size();
         ArrayList<Fragment> fragments = new ArrayList<>(count);
-        for (int i = 0; i < count; i++) {
-            fragments.add(active.valueAt(i));
+        for (String key : active.keySet()) {
+            fragments.add(active.get(key));
         }
         return fragments;
     }
 
-    private static Object getValue(Object object, String fieldName) throws Exception {
+    private static Object getValue(Object object, String fieldName) {
         Field field;
         Class<?> clazz = object.getClass();
         try {
@@ -183,8 +194,9 @@ public class FragmentationMagician {
     }
 
     private static void hookStateSaved(FragmentManager fragmentManager, Runnable runnable) {
-        if (!(fragmentManager instanceof FragmentManagerImpl)) return;
-
+        if (!(fragmentManager instanceof FragmentManagerImpl)) {
+            return;
+        }
         FragmentManagerImpl fragmentManagerImpl = (FragmentManagerImpl) fragmentManager;
         if (isStateSaved(fragmentManager)) {
             fragmentManagerImpl.mStateSaved = false;
@@ -202,7 +214,8 @@ public class FragmentationMagician {
      *
      * @see FragmentManager#isStateSaved()
      */
-    private static void compatRunAction(FragmentManagerImpl fragmentManagerImpl, Runnable runnable) {
+    private static void compatRunAction(FragmentManagerImpl fragmentManagerImpl, Runnable
+            runnable) {
         if (!sSupportGreaterThan27dot1dot0) {
             runnable.run();
             return;
